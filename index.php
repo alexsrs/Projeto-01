a49924fe6357c8d4bef96130ee613cb9e6636def
Alex Sandro <alex@creapix.com.br>
2021-05-03 08:48:55 -0300

    pagina de contato

--- index.php
+++ index.php
@@ -1,122 +1,94 @@
+<?php include('config.php'); ?>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Projeto 01 - PHP</title>
-	<link href="css/fontawesome.min.css" rel="stylesheet">
- 	<link href="css/brands.min.css" rel="stylesheet">
-	<link href="css/solid.min.css" rel="stylesheet">
+	<link href="<?php echo INCLUDE_PATH; ?>css/fontawesome.min.css" rel="stylesheet">
+ 	<link href="<?php echo INCLUDE_PATH; ?>css/brands.min.css" rel="stylesheet">
+	<link href="<?php echo INCLUDE_PATH; ?>css/solid.min.css" rel="stylesheet">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="estilo/style.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo INCLUDE_PATH; ?>estilo/style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="keywords" content="palavras-chave,do,meu,site">
 	<meta name="description" content="Descrição do meu website">
 	<meta charset="utf-8" />
 </head>
 <body>
+	<?php 
+		$url = isset($_GET['url']) ? $_GET['url'] : 'home';
+		switch ($url) {
+			case 'sobre':
+				echo '<target="sobre" />';
+				# code...
+				break;
+			case 'servicos':
+				echo '<target="servicos" />';
+				# code...
+				break;
+		
+		}
+	?>
+
 	<header>
 		<div class="center">
-			<div class="logo left"><a href="/">Solid FIT</a></div><!-- logo -->
+			<div class="logo left"><a href="<?php echo INCLUDE_PATH; ?>">Solid FIT</a></div><!-- logo -->
 			<nav class="desktop right">
 				<ul>
-					<li><a href="">Home</a></li>
-					<li><a href="">Sobre</a></li>
-					<li><a href="">Serviços</a></li>
-					<li><a href="">Contato</a></li>
+					<li><a href="<?php echo INCLUDE_PATH; ?>">Home</a></li>
+					<li><a href="<?php echo INCLUDE_PATH; ?>sobre">Sobre</a></li>
+					<li><a href="<?php echo INCLUDE_PATH; ?>servicos">Serviços</a></li>
+					<li><a href="<?php echo INCLUDE_PATH; ?>contato">Contato</a></li>
 				</ul>
 			</nav>
 			<nav class="mobile right">
 				<div class="botao-menu-mobile"><i class="fas fa-align-justify"></i></div>
 				<ul>
-					<li><a href="">Home</a></li>
-					<li><a href="">Sobre</a></li>
-					<li><a href="">Serviços</a></li>
-					<li><a href="">Contato</a></li>
+					<li><a href="<?php echo INCLUDE_PATH; ?>">Home</a></li>
+					<li><a href="<?php echo INCLUDE_PATH; ?>sobre">Sobre</a></li>
+					<li><a href="<?php echo INCLUDE_PATH; ?>servicos">Serviços</a></li>
+					<li><a href="<?php echo INCLUDE_PATH; ?>contato">Contato</a></li>
 				</ul>
 			</nav>
 			<div class="clear"></div><!-- clear -->
 		</div><!-- center -->
 	</header>
-	<section class="banner-principal">
-		<div class="overlay"></div><!--overlay-->
-		<div class="center">
-			<form>
-				<h2>Qual o seu melhor e-mail</h2>
-				<input type="email" name="email" required />
-				<input type="submit" name="acao" value="Cadastrar">
-			</form>
-		</div><!-- center -->
-	</section><!--banner-principal-->
-	<section class="descricao-autor">
-		<div class="center">
-			<div class="w50 left">
-				<h2>Alex Sandro R. de Souza</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus. In ac purus congue, aliquam orci eu, dignissim massa. Duis tincidunt orci ut cursus mollis. Vivamus nec lacus diam. Praesent dictum sed ante non blandit. Vestibulum diam ante, dignissim ut rhoncus et, faucibus non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in enim lacus. Nullam efficitur nisl vel arcu vehicula, vitae auctor nibh convallis. In hac habitasse platea dictumst. Integer porttitor dolor ac bibendum viverra. Nunc luctus aliquam neque vitae suscipit. Etiam varius, diam eu iaculis sollicitudin, velit dui porttitor ante, in semper risus leo id turpis.</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus. In ac purus congue, aliquam orci eu, dignissim massa. Duis tincidunt orci ut cursus mollis. Vivamus nec lacus diam. Praesent dictum sed ante non blandit. Vestibulum diam ante, dignissim ut rhoncus et, faucibus non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in enim lacus. Nullam efficitur nisl vel arcu vehicula, vitae auctor nibh convallis. In hac habitasse platea dictumst. Integer porttitor dolor ac bibendum viverra. Nunc luctus aliquam neque vitae suscipit. Etiam varius, diam eu iaculis sollicitudin, velit dui porttitor ante, in semper risus leo id turpis.</p>
-			</div><!-- w50 -->
-			<div class="w50 left">
-				<img class="right" src="images/eu.jpg">
-			</div><!-- w50 -->
-			<div class="clear"></div><!-- clear --> 
-		</div><!-- center -->
-	</section><!--descricao-autor-->
-	<section class="especialidades">
-		<div class="center">
-			<h2 class="title">Especialidades</h2>
-			<div class="box-especialidade w33 left">
-				<h3><i class="fab fa-css3"></i></h3>
-				<h4>CSS3</h4>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus.</p>
-			</div><!-- box-especialidade -->
-			<div class="box-especialidade w33 left">
-				<h3><i class="fab fa-html5"></i></h3>
-				<h4>HTML5</h4>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus.</p>
-			</div><!-- box-especialidade -->
-			<div class="box-especialidade w33 left">
-				<h3><i class="fab fa-js-square"></i></h3>
-				<h4>JavaScript</h4>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus.</p>
-			</div><!-- box-especialidade -->
-			<div class="clear"></div><!--clear-->
-		</div><!-- center -->
-	</section><!-- especialidades -->
-	<section class="extras">
-		<div class="center">
-			<div class="w50 left depoimentos-container">
-				<h2 class="title">Depoimentos dos nossos clientes</h2>
-					<div class="depoimento-single">
-						<p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus."</p>
-						<p class="nome-autor">Lorem ipsum dolor</p>
-					</div><!-- depoimento-single -->
-					<div class="depoimento-single">
-						<p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus."</p>
-						<p class="nome-autor">Lorem ipsum dolor</p>
-					</div><!-- depoimento-single -->
-					<div class="depoimento-single">
-						<p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus."</p>
-						<p class="nome-autor">Lorem ipsum dolor</p>
-					</div><!-- depoimento-single -->
-			</div><!-- w50 -->
-			<div class="w50 left servicos-container">
-				<h2 class="title">Serviços</h2>
-				<div class="servicos">
-					<ul>
-						<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus.</li>
-						<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus.</li>
-						<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus.</li>
-						<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus.</li>
-					</ul>
-				</div><!-- servicos -->
-			</div><!-- w50 -->
-			<div class="clear"></div><!-- clear --> 
-		</div><!-- center -->
-	</section><!-- extras -->
+<?php 
+	
+
+	if(file_exists('pages/'.$url.'.php')){
+		include('pages/'.$url.'.php');
+	} else {
+		// podemos fazer o que quiser poir a pagina nao existe
+		if ($url != 'sobre' && $url != 'servicos') {
+			$pagina404 = true;
+			include('pages/404.php');
+
+			# code...
+		} else {
+			include('pages/home.php');
+		}
+		
+	}
+
+
+?>
 	<footer>
 		<div class="center">
 			<p>Solid Fit - Todos os direitos reservados</p>
 		</div><!-- center -->	
 	</footer>
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-	<script src="js/scripts.js"></script>
+	<script src="<?php echo INCLUDE_PATH; ?>js/scripts.js"></script>
+
+	<?php if($url == 'contato'){
+
+	}
+	?>
+
+	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyCnh9a-0dAbErxqWnQiJQqfL4DU_9YLEb8"></script>
+	<script src="<?php echo INCLUDE_PATH;?>js/initMap.js"></script>
 </body>
 </html>
+
+
---
+++ pages/home.php
@@ -0,0 +1,76 @@
+
+	<section class="banner-principal">
+		<div class="overlay"></div><!--overlay-->
+		<div class="center">
+			<form>
+				<h2>Qual o seu melhor e-mail</h2>
+				<input type="email" name="email" required />
+				<input type="submit" name="acao" value="Cadastrar">
+			</form>
+		</div><!-- center -->
+	</section><!--banner-principal-->
+	<section class="descricao-autor">
+		<div class="center">
+			<div class="w50 left">
+				<h2>Alex Sandro R. de Souza</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus. In ac purus congue, aliquam orci eu, dignissim massa. Duis tincidunt orci ut cursus mollis. Vivamus nec lacus diam. Praesent dictum sed ante non blandit. Vestibulum diam ante, dignissim ut rhoncus et, faucibus non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in enim lacus. Nullam efficitur nisl vel arcu vehicula, vitae auctor nibh convallis. In hac habitasse platea dictumst. Integer porttitor dolor ac bibendum viverra. Nunc luctus aliquam neque vitae suscipit. Etiam varius, diam eu iaculis sollicitudin, velit dui porttitor ante, in semper risus leo id turpis.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus. In ac purus congue, aliquam orci eu, dignissim massa. Duis tincidunt orci ut cursus mollis. Vivamus nec lacus diam. Praesent dictum sed ante non blandit. Vestibulum diam ante, dignissim ut rhoncus et, faucibus non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in enim lacus. Nullam efficitur nisl vel arcu vehicula, vitae auctor nibh convallis. In hac habitasse platea dictumst. Integer porttitor dolor ac bibendum viverra. Nunc luctus aliquam neque vitae suscipit. Etiam varius, diam eu iaculis sollicitudin, velit dui porttitor ante, in semper risus leo id turpis.</p>
+			</div><!-- w50 -->
+			<div class="w50 left">
+				<img class="right" src="<?php echo INCLUDE_PATH; ?>images/eu.jpg">
+			</div><!-- w50 -->
+			<div class="clear"></div><!-- clear --> 
+		</div><!-- center -->
+	</section><!--descricao-autor-->
+	<section class="especialidades">
+		<div class="center">
+			<h2 class="title">Especialidades</h2>
+			<div class="box-especialidade w33 left">
+				<h3><i class="fab fa-css3"></i></h3>
+				<h4>CSS3</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus.</p>
+			</div><!-- box-especialidade -->
+			<div class="box-especialidade w33 left">
+				<h3><i class="fab fa-html5"></i></h3>
+				<h4>HTML5</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus.</p>
+			</div><!-- box-especialidade -->
+			<div class="box-especialidade w33 left">
+				<h3><i class="fab fa-js-square"></i></h3>
+				<h4>JavaScript</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus.</p>
+			</div><!-- box-especialidade -->
+			<div class="clear"></div><!--clear-->
+		</div><!-- center -->
+	</section><!-- especialidades -->
+	<section class="extras">
+		<div class="center">
+			<div class="w50 left depoimentos-container">
+				<h2 class="title">Depoimentos dos nossos clientes</h2>
+					<div class="depoimento-single">
+						<p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus."</p>
+						<p class="nome-autor">Lorem ipsum dolor</p>
+					</div><!-- depoimento-single -->
+					<div class="depoimento-single">
+						<p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus."</p>
+						<p class="nome-autor">Lorem ipsum dolor</p>
+					</div><!-- depoimento-single -->
+					<div class="depoimento-single">
+						<p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus."</p>
+						<p class="nome-autor">Lorem ipsum dolor</p>
+					</div><!-- depoimento-single -->
+			</div><!-- w50 -->
+			<div class="w50 left servicos-container">
+				<h2 class="title">Serviços</h2>
+				<div class="servicos">
+					<ul>
+						<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus.</li>
+						<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus.</li>
+						<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus.</li>
+						<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut eros vel tortor blandit ultrices ac ac justo. Nulla euismod diam id risus maximus rutrum. Integer mollis diam ex, vel molestie orci scelerisque a. Phasellus est risus, pulvinar id volutpat mattis, luctus accumsan purus.</li>
+					</ul>
+				</div><!-- servicos -->
+			</div><!-- w50 -->
+			<div class="clear"></div><!-- clear --> 
+		</div><!-- center -->
+	</section><!-- extras -->
