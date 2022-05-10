56ffa8cdedfa596f42d2277d0e70fffc17e4f0d6
Alex Sandro <alex@creapix.com.br>
2022-05-10 12:06:45 -0300

    tentei mais nao foi
    subindo codigo mesmo com erro

--- classes/Email.php
+++ classes/Email.php
@@ -9,7 +9,7 @@
 		{
 			// code...
 			//Create an instance; passing `true` enables exceptions
-			$mail = new PHPMailer(true);
+			$mail = new PHPMailer();
 
 			try {
 			    //Server settings
--- config.php
+++ config.php
@@ -1,26 +1,14 @@
 <?php
 
 	
+define('INCLUDE_PATH','http://localhost/php-projetos/Projeto-01/');
+
+	require 'classes/vendor/autoload.php';
 	//Carregar classes 	
-	$autoload = function($class){
-
-		//Carregar php mailer
-		if($class =='Email'){
-			include('classes/vendor/autoload.php');
-			
-		};
-
-
-
-	
+$autoload = function($class){
 		include('classes/'.$class.'.php');
 	};
 
 	spl_autoload_register($autoload);
-	// fim da função de carregamento dinamico 
-
-
-	define('INCLUDE_PATH','http://localhost/php-projetos/Projeto-01/');
-
 	
 ?>
