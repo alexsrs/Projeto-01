711115ec8c5191f8b070cdd5f30347f02bcefb9d
Alex Sandro <alex@creapix.com.br>
2023-03-28 18:57:03 -0300

    implementando a interface de login

--- painel/login.php
+++ painel/login.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Painel de controle</title>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
     <link href="<?php echo INCLUDE_PATH_PAINEL ?>css/style.css" rel="stylesheet"/>
 </head>
 <body>  
     <div class="box-login">
+        <h2>Faça o login:</h2>
         <form method="post">
             <input type="text" name="user" placeholder="Login..." required>
             <input type="password" name="password" placeholder="Senha..." required>
