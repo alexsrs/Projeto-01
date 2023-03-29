dcbf199ae7932e7dce07effaaa47d74697836784
Alex Sandro <alex@creapix.com.br>
2023-03-29 15:01:58 -0300

    atualizado o sql com os novos campos do BD

--- projeto-01.sql
+++ projeto-01.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 28, 2023 at 11:03 PM
+-- Generation Time: Mar 29, 2023 at 06:01 PM
 -- Server version: 8.0.31
 -- PHP Version: 7.4.33
 
@@ -32,6 +32,9 @@ CREATE TABLE IF NOT EXISTS `tb_admin.usuarios` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `cargo` int NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 
@@ -39,8 +42,8 @@ CREATE TABLE IF NOT EXISTS `tb_admin.usuarios` (
 -- Dumping data for table `tb_admin.usuarios`
 --
 
-INSERT INTO `tb_admin.usuarios` (`id`, `user`, `password`) VALUES
-(1, 'admin', 'admin');
+INSERT INTO `tb_admin.usuarios` (`id`, `user`, `password`, `img`, `nome`, `cargo`) VALUES
+(1, 'admin', 'admin', 'avatar-teste.jpg', 'Alex Sandro', 2);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
