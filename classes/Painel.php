d1e5b155ff856d6fb08558794641d487f6d57990
Alex Sandro <alex@creapix.com.br>
2023-03-29 11:45:33 -0300

    Implementando o logout

--- classes/Painel.php
+++ classes/Painel.php
@@ -3,5 +3,10 @@
         public static function logado(){
             return isset($_SESSION['login']) ? true : false;
         }
+
+        public static function logout(){
+            session_destroy();
+            header('location: '.INCLUDE_PATH_PAINEL);
+        }
     }
 ?>
--- painel/main.php
+++ painel/main.php
@@ -1,1 +1,40 @@
-<h2>Bem-vindo ao painel, <?php echo $_SESSION['user']; ?> </h2>
+<?php 
+    if(isset($_GET['logout'])){
+        Painel::logout();
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel de controle</title>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+    <link href="<?php echo INCLUDE_PATH; ?>css/fontawesome.min.css" rel="stylesheet">
+ 	<link href="<?php echo INCLUDE_PATH; ?>css/brands.min.css" rel="stylesheet">
+	<link href="<?php echo INCLUDE_PATH; ?>css/solid.min.css" rel="stylesheet">
+    <link href="<?php echo INCLUDE_PATH_PAINEL ?>css/style.css" rel="stylesheet"/>
+
+</head>
+<body>  
+<div class="menu">
+
+</div><!-- menu -->
+
+    <header> 
+        <div class="center">
+            <div class="logout">
+                <a href="<?php echo INCLUDE_PATH_PAINEL ?>?logout"><i class="fa fa-window-close"></i></a>
+            </div><!-- logout -->
+            <div class="clear"></div><!-- clear -->
+        </div> <!-- center -->
+    </header>
+</body>
+</html>
+
+
+
+
+
